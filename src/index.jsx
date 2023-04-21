85cc2dcd2f664548cbe14705fca87d3a4dd80b03
validzed <validtry@gmail.com>
2023-04-22 00:41:35 +0700

    Add components to displays the contact list

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ContactApp from './components/ContactApp';
+import './styles/style.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ContactApp />);
